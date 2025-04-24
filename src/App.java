345b7ec2bdfb9d45828c0cfa06f3c3a5ad97bd00
igorm_dev <igoribeirom@gmail.com>
2025-04-24 19:26:50 -0300

    feat: init project

---
+++ src/App.java
@@ -0,0 +1,25 @@
+/*
+ * Integrantes do grupo:
+ * - Igor Ribeiro Machado
+ * - Luiz Augusto Santos Gumz
+ * - Luis Henrique Silva de Lima
+ */
+
+public class App {
+    /*
+     * O que deve conter no sistema ?
+     * 
+     * 1. Sistema de menu
+     * 2. Cadastro de livros
+     * 3. Pesquisa de livros
+     * 4. cadastro de usuarios
+     * 5. empréstimo de livros
+     * 6. devolução de livros
+     * 7. atrasos
+     * 8. relatórios
+     * 9. pré carga dos dados para teste
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Inicializando projeto da biblioteca!");
+    }
+}
